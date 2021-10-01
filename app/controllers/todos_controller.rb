36c694b0d2f5c1c3058436ca96a2488a153989bb
kemmo47 <longkem123456@gmail.com>
2021-10-01 16:22:27 +0700

    test1

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -27,6 +27,7 @@ class TodosController < ApplicationController
     respond_to do |format|
       format.html { redirect_to todos_url, notice: "Todo was successfully destroyed." }
       format.json { head :no_content }
+      format.js
     end
   end
 
