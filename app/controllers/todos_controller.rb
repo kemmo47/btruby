758dc0e983ded7dbd8e05048623b7089e7b2a38d
kemmo47 <longkem123456@gmail.com>
2021-10-03 21:45:13 +0700

    da xong, chua them docker

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,15 +1,34 @@
 class TodosController < ApplicationController
   before_action :set_todo, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: [:index]
+  before_action :correct_user, only: [:destroy]
 
   # GET /todos or /todos.json
   def index
-    @todos = Todo.all
-    @todo = Todo.new
+    @todos =  if params[:info_id]
+                  if params[:info_id] === '0'
+                    Todo.all                       
+                  else
+                    Todo.where( user_id: params[:info_id] )
+                  end
+              else
+                Todo.all
+              end       
+    # @todos = Todo.where(user_id: current_user.id)
+    # @todo = Todo.new
+    @todo = current_user.todos.build
+    @users = User.all
+        
   end
+
+  # def show
+  #   @todos = Todo.find(params[:id])
+  # end
 
   # POST /todos or /todos.json
   def create
-    @todo = Todo.new(todo_params)
+    # @todo = Todo.new(todo_params)
+    @todo = current_user.todos.build(todo_params)
 
     respond_to do |format|
       if @todo.save
@@ -29,6 +48,15 @@ class TodosController < ApplicationController
       format.json { head :no_content }
       format.js
     end
+  end
+
+  def correct_user
+    @todo = current_user.todos.find_by(id: params[:id])
+    redirect_to todos_path, notice: "Not Authorized To Edit This Todo" if @todo.nil?
+  end
+  
+  def infouser_params
+    params.require().permit(:info_id)
   end
 
   private
